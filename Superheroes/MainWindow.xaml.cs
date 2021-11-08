c4d14d71b04ead9c6a60a2768e5b7e10fdc4beba
Marga Arroyo <margarita.arroyo@iesdoctorbalmis.com>
2021-11-08 15:00:36 +0100

    Agregar archivos de proyecto.

---
+++ Superheroes/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Superheroes
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int numeroSuperheroe = 0;
+        List<Superheroe> superLista;
+        public MainWindow()
+        {
+            InitializeComponent();
+            superLista = Superheroe.GetSamples();
+
+            principalDockPanel.DataContext = superLista[numeroSuperheroe];
+
+        }
+
+        private void Siguiente(object sender, RoutedEventArgs e)
+        {
+            Button boton = (Button)sender;
+            
+
+            if (boton.Tag.Equals("siguiente") && (numeroSuperheroe<2))
+            {
+                numeroSuperheroe++;
+                numeroImagenTextBlock.Text = (numeroSuperheroe+1) +"/3";
+                principalDockPanel.DataContext = superLista[numeroSuperheroe];
+            }
+            else if(boton.Tag.Equals("atras") && (numeroSuperheroe > 0))
+            {
+                numeroSuperheroe--;
+                numeroImagenTextBlock.Text = (numeroSuperheroe + 1)+ "/3";
+                principalDockPanel.DataContext = superLista[numeroSuperheroe];
+            }
+                
+        }
+    }
+}
---
+++ Superheroes/Superheroe.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheroes 
+{
+    class Superheroe  : INotifyPropertyChanged
+
+    {
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value;
+                this.NotifyPropertyChanged("Nombre");
+            }
+        }
+        private string imagen;
+
+        public string Imagen
+        {
+            get { return imagen; }
+            set
+            {
+                imagen = value;
+                this.NotifyPropertyChanged("Imagen");
+            }
+        }
+
+
+        private bool vengador;
+
+        public bool Vengador
+        {
+            get { return vengador; }
+            set
+            {
+                vengador = value;
+                this.NotifyPropertyChanged("Vengador");
+            }
+        }
+        
+        private bool xmen;
+
+        public bool Xmen
+        {
+            get { return xmen; }
+            set
+            {
+                xmen = value;
+                this.NotifyPropertyChanged("Xmen");
+            }
+        }
+        private bool heroe;
+        public bool Heroe
+        {
+            get { return heroe; }
+            set
+            {
+                heroe = value;
+                this.NotifyPropertyChanged("Heroe");
+            }
+        }
+
+
+        public Superheroe()
+        {
+        }
+
+        public Superheroe(string nombre, string imagen, bool vengador, bool xmen, bool heroe)
+        {
+            Nombre = nombre;
+            Imagen = imagen;
+            Vengador = vengador;
+            Xmen = xmen;
+            Heroe = heroe;
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName) 
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+        public static List<Superheroe> GetSamples()
+        {
+            List<Superheroe> ejemplos = new List<Superheroe>();
+
+            Superheroe ironman = new Superheroe("Ironman", @"https://dossierinteractivo.com/wp-content/uploads/2021/01/Iron-Man.png", true, false, true);
+            Superheroe kingpin = new Superheroe("Kingpin", @"https://www.comicbasics.com/wp-content/uploads/2017/09/Kingpin.jpg", false, false, false);
+            Superheroe spiderman = new Superheroe("Spiderman", @"https://wipy.tv/wp-content/uploads/2019/08/destino-de-%E2%80%98Spider-Man%E2%80%99-en-los-Comics.jpg", true, true, true);
+
+            ejemplos.Add(ironman);
+            ejemplos.Add(kingpin);
+            ejemplos.Add(spiderman);
+
+            return ejemplos;
+        }
+    }
+}
